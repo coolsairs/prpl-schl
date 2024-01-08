01562b4b6fd6ec72d65e64d29c6391d4517ff32d
coolsairs <124494018+coolsairs@users.noreply.github.com>
2024-01-08 18:02:09 +0300

    4-navigator

---
+++ script.js
@@ -0,0 +1,15 @@
+// let adressLat = 10;
+// let adressLong = 12;
+// let positionLat = 11;
+// let positionLong = 13;
+
+// const wayLength = Math.sqrt(((adressLat - positionLat) ** 2) + (adressLong - positionLong) ** 2);
+
+// console.log(wayLength);
+
+const wayLength = (adressLat, adressLong, positionLat, positionLong ) => {
+    const pathCalculation = Math.sqrt(((adressLat - positionLat) ** 2) + (adressLong - positionLong) ** 2);
+    return pathCalculation;
+}
+
+console.log(wayLength(111, 122, 11, 132));
