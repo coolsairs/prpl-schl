24a70fe43c5c386aec738c1d3648bb0b8de76c80
coolsairs <124494018+coolsairs@users.noreply.github.com>
2024-01-14 14:05:58 +0300

    Purple school homework
    4 - navigator; 5 - languages; 6 - driver

---
+++ 5 - languages/script.js
@@ -0,0 +1,17 @@
+let language = 'ga';
+ switch (language) {
+    case 'de':
+        console.log('God morgen!');
+        break;
+    case 'fr':
+        console.log('Bon après-midi!');
+        break;
+    case 'el':
+        console.log('Καλό απόγευμα!');
+        break;
+    case 'ga':
+        console.log('Tráthnóna maith!');
+        break;
+    default:
+        console.log('Пока нет приветствия на данном языке');
+ } 
