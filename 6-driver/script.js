749d80b1b3a046a4314150f5d27186a1db1323c4
coolsairs <124494018+coolsairs@users.noreply.github.com>
2024-01-14 14:07:55 +0300

    Delete 6-driver directory

--- 6-driver/script.js
+++
@@ -1,6 +0,0 @@
-hasLisence = true;
-age = 18;
-isDrunk = false;
-
-const isDrivingAvailable = (hasLisence === true && age >= 18 && isDrunk === false) ? 'может' : 'не может';
-console.log(isDrivingAvailable);
